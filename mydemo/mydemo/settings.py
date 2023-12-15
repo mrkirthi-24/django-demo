fc3ca41b9af7ae39d599da6f2e01d005d821cee8
mrkirthi-24 <kirthibagrecha4@gmail.com>
2023-12-15 21:44:01 +0530

    connect to my PostgreSQL DB

--- mydemo/mydemo/settings.py
+++ mydemo/mydemo/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "myapp",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -75,9 +77,17 @@ WSGI_APPLICATION = "mydemo.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "hercules",
+        "USER": "hercules",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
