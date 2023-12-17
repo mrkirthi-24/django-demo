e55c8ec7ac6da36feded98b48f316ea4891721d9
mrkirthi-24 <kirthibagrecha4@gmail.com>
2023-12-17 15:08:02 +0530

    Delete post and Ban user functionality

--- mydemo/myapp/views.py
+++ mydemo/myapp/views.py
@@ -1,14 +1,40 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User, Group
 from django.shortcuts import redirect, render
 from .models import TodoItem, Post
 from .forms import PostForm, RegisterForm
 
 # Create your views here.
+
 @login_required(login_url="/login")
 def home(request):
     posts = Post.objects.all()
-    return render(request, "home.html", {"posts": posts})
+
+    if request.method == "POST":
+        post_id = request.POST.get("post-id")
+        user_id = request.POST.get("user-id")
+
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm("main.delete_post")):
+                post.delete()
+        elif user_id:
+            user = User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.get(name='default')
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+                try:
+                    group = Group.objects.get(name='mod')
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+    return render(request, 'home.html', {"posts": posts})
 
 
 def todos(request):
@@ -17,6 +43,7 @@ def todos(request):
 
 
 @login_required(login_url="/login")
+@permission_required("myapp.add_post", login_url="/login", raise_exception=True)
 def create_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
