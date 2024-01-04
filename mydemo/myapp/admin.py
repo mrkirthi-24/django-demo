d18be228a23b58a9d0b7bf7bf785c168be5ae3ee
mrkirthi-24 <kirthibagrecha4@gmail.com>
2024-01-04 12:28:54 +0530

    update company and employee admin

--- mydemo/myapp/admin.py
+++ mydemo/myapp/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from .models import TodoItem, Post, Employee, Company
 
 # Register your models here to view and alter then in django admin.
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    search_fields = ('name',)
+ 
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name',)
+    list_filter = ('company',)
+
 admin.site.register(TodoItem)
 admin.site.register(Post)
-admin.site.register(Company)
-admin.site.register(Employee)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Employee, EmployeeAdmin)
